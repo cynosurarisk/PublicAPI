629553d3d679c1cd9d7f0a3dd4ab12c3a375521b
Patrice LaFond <76969068+patrice-cynosura@users.noreply.github.com>
2021-03-31 15:48:00 -0400

    Create samples.cs

---
+++ samples.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
